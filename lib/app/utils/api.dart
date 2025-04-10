dd940edcf975427ccddb74aa2a9df9bc9f63b401
sindylauna <sindylaunapuspasari@gmail.com>
2025-04-10 14:49:52 +0700

    keuangan

--- lib/app/utils/api.dart
+++ lib/app/utils/api.dart
@@ -1,5 +1,7 @@
 class BaseUrl {
-  static String login = 'http://192.168.130.134:8000/api/login';
-  static String jenisumkms = 'http://192.168.130.134:8000/api/jenisumkm';
+  static String login = 'http://192.168.164.134:8000/api/login';
+  static String jenisumkms = 'http://192.168.164.134:8000/api/jenisumkm';
+  static String operasional = 'http://192.168.164.134:8000/api/operasional';
+  
 
 }
